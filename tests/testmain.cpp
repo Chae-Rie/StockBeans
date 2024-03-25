658e3eeb15a4b0b41ff9851941edde7205a38327
yoocherry <yoospam@protonmail.com>
2024-03-25 22:36:12 +0100

    Anpassung der CMakeLists.txt und Hinzufügen der Testmain

---
+++ tests/testmain.cpp
@@ -0,0 +1,10 @@
+
+int main(int argc, char *argv[])
+{
+    // // Testen der Datenbankverbindung, ganz fundamental
+    // TestDatabaseConnection testDbConnection;
+    // QTest::qExec(&testDbConnection, argc, argv);
+
+    // Testdatabasequeries testDbQueries;
+    // QTest::qExec(&testDbQueries, argc, argv);
+}
