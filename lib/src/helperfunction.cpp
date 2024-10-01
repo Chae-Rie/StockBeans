36c01f09741edd006e6050b138e3cd84e9e7455e
yoocherry <yoospam@protonmail.com>
2024-10-01 21:32:39 +0200

    Hinzufügen von Unittests für die Kalkulation des SHA256 Hashs

--- lib/src/helperfunction.cpp
+++ lib/src/helperfunction.cpp
@@ -1,19 +1,20 @@
 #include "helperfunction.h"
 
 
-QString HelperFunction::GetCurrentTime()
-{
+QString HelperFunction::GetCurrentTime() {
     QDateTime date = QDateTime::currentDateTime();
     QString formattedTime = date.toString("yyyy-MM-dd HH:mm:ss");
     return formattedTime;
 }
 
 
-QString HelperFunction::CalculateSHA256Hash(const QString &input)
-{
-    QByteArray byteArray = input.toUtf8(); // Konvertiere den Eingabestring in QByteArray
+QString HelperFunction::CalculateSHA256Hash(const QString &input) {
+    if (input.isEmpty()) {
+        return {};
+    }
+    QByteArray byteArray = input.toUtf8(); // fills a QByteArray with the utf8 encoded input
     QByteArray hashArray = QCryptographicHash::hash(byteArray, QCryptographicHash::Sha256); // Berechne den SHA-256-Hash
-    return QString(hashArray.toHex()); // Konvertiere den Hash in einen Hexadezimal-String und gebe ihn zurück
-
+    return hashArray.toHex(); // returns hash array as a converted hexvalue
 }
+
 
---
+++ tests/helperfunction_test.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Yoo Cherry on 01.10.24.
+//
+
+#include <gtest/gtest.h>
+#include "../lib/src/helperfunction.h"
+
+class HelperFunctionTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+
+TEST_F(HelperFunctionTest, CalculateSHA256Hash_EmptyInput) {
+    QString input = "";
+    QString expectedOutput = "";
+    ASSERT_EQ(HelperFunction::CalculateSHA256Hash(input), expectedOutput);
+}
+
+TEST_F(HelperFunctionTest, CalculateSHA256Hash_DifferentCharsets) {
+    QString input = "你好, 世界!"; // "Hello, World!" auf Chinesisch
+    QString expectedOutput = "12477654131605bfc19ba5a3862ac54f18f9140509fcb3c1faad687c75ff64d1";
+    ASSERT_EQ(HelperFunction::CalculateSHA256Hash(input), expectedOutput);
+}
+
+TEST_F(HelperFunctionTest, CalculateSHA256Hash_NumericInput) {
+    QString input = "1234567890";
+    QString expectedOutput = "c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646";
+    ASSERT_EQ(HelperFunction::CalculateSHA256Hash(input), expectedOutput);
+}
+
+TEST_F(HelperFunctionTest, CalculateSHA256Hash_SpecialCharacters) {
+    QString input = "!@#$%^&*()_+";
+    QString expectedOutput = "36d3e1bc65f8b67935ae60f542abef3e55c5bbbd547854966400cc4f022566cb";
+    ASSERT_EQ(HelperFunction::CalculateSHA256Hash(input), expectedOutput);
+}
+
